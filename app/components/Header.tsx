cdba3c7e84ed52bb358c4a907fbf4dba57b03c96
Muhammed Haneen Ep <159922620+haneenep@users.noreply.github.com>
2025-11-17 15:10:59 +0530

    fix: menu button mobile screen

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -70,7 +70,7 @@ const Header: React.FC = () => {
 
   return (
       <div
-        className="fixed top-2 left-2 md:left-10 md:right-10 right-2 z-50 mx-2 transition-all duration-300 overflow-hidden"
+        className="fixed top-2 left-2 md:left-10 md:right-10 right-2 z-50 mx-2 transition-all duration-300"
         style={{ 
           backgroundColor: isScrolled ? 'rgba(0, 0, 0, 0.6)' : 'rgba(0, 0, 0, 1)',
           backdropFilter: isScrolled ? 'blur(15px)' : 'none',
